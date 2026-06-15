f8b16c78067e54881c96c0850240cf5733b9b373
BhavaniGIS <bhavanidundi1@gmail.com>
2026-06-15 12:18:55 +0530

    Create Day 5_Modules_and_Strings.py

---
+++ Day 5_Modules_and_Strings.py
@@ -0,0 +1,27 @@
+# DAY 5: Imports & Math Modules
+# ==============================================================================
+
+# 14. KEYWORD: import
+# Core Use: Loading external modules or standard libraries to extend functionality.
+# Explanation: Grants access to code, functions, and variables defined in an external python file or module.
+
+import math 
+print(f"Square root of 16 is: {math.sqrt(16)}")
+
+
+# 15. KEYWORDS: If 
+# Core Use: Calculating the exact square root of a given numeric value.
+# Explanation: A function bundled inside the 'math' module that returns a floating-point square root 
+result.
+
+total_count  = 349
+If total_count > 100:
+   print(“This is a big zone”)
+
+# 16. DATA TYPE: String (str)
+# Core Use: Storing textual data or characters in a variable.
+# Explanation: Any text enclosed within single or double quotes is recognized as a String in core 
+
+shop_name = "Hyderabad"
+print(shop_name)       
+print(type(shop_name)) 
